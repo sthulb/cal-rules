3a0ce0b030f5adffbc35cba9c8acfb44b72cce1e
Simon <sthulb@users.noreply.github.com>
2021-05-12 12:29:47 +0100

    fix rootca class to allow for param

--- calrules/exchange_rootca.py
+++ calrules/exchange_rootca.py
@@ -1,12 +1,8 @@
-from urllib3 import connection
 from requests import adapters
 
 class ExchangeRootCA(adapters.HTTPAdapter):
-  ca_cert: str
+    ca_cert: str
 
-  def __init__(self, ca_cert: str):
-    self.ca_cert = ca_cert
-
-  class Root(adapters.HTTPAdapter):
-    def cert_verify(self, conn, url, verify, cert):
-      return super().cert_verify(conn=conn, url=url, verify="root-x509.cer", cert=cert)
+    def cert_verify(self, conn, url, verify: str, cert):
+        verify = self.ca_cert
+        return super().cert_verify(conn=conn, url=url, verify=verify, cert=cert)
