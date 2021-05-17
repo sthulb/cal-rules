2c36b958cd531c9aa8752017da248ad219374cf8
Simon <sthulb@users.noreply.github.com>
2021-05-17 15:27:17 +0100

    add better logging and response actions

--- calrules/cli.py
+++ calrules/cli.py
@@ -1,8 +1,66 @@
 #!/usr/bin/env python
 
+import logging
+import os
+
 from calrules.config import loads_config, ConfigError
 from calrules.exchange import Exchange
+from calrules.rules import Response, Rules, RuleRuntimeError
+from calrules.item import ItemActionError
+
+for name in logging.root.manager.loggerDict:
+    if not name.startswith("calrules"):
+        logging.getLogger(name).setLevel(logging.WARNING)
+
+logging.basicConfig(
+    format="%(asctime)s — %(name)s — %(levelname)s — %(message)s",
+    level=os.environ.get("LOG_LEVEL", "INFO").upper()
+)
+
+LOG = logging.getLogger(__name__)
 
 def main():
-  config = loads_config("./config.yaml")
-  exchange = Exchange(config["exchange"])
+    LOG.info("Starting to scan mailbox")
+    try:
+        config = loads_config("./config.yaml")
+        LOG.debug(f"Loaded config: {config}")
+    except ConfigError as e:
+        LOG.error(f"Unable to parse config: {e}")
+
+    exchange = Exchange(config["exchange"])
+    items = exchange.items()
+    rules = Rules(config["rules"])
+
+    for i in items:
+        for r in rules:
+            try:
+                match = r.matches(i.to_dict())
+            except RuleRuntimeError as e:
+                LOG.error(f"There was a problem running this rule: {r.description}")
+                continue
+
+            LOG.info(f"Checking {i.subject} against {r.pattern}: {match}")
+            LOG.debug("Item attributes: %s", i.to_dict())
+
+            if match and i.action is not None:
+                i.mark(r.response, r.message)
+
+    for i in items:
+        if i.action is Response.NOOP:
+            continue
+
+        LOG.info(f"Item: {i.subject} is marked for {i.action}")
+
+        try:
+            if i.action is Response.ACCEPT:
+                i.accept()
+            elif i.action is Response.MAYBE:
+                i.maybe()
+            elif i.action is Response.DECLINE:
+                i.decline()
+            elif i.action is Response.DELETE:
+                i.delete()
+            else:
+                LOG.warn(f"Unsure what to do, subject: {i.subject}, action: {i.action}")
+        except ItemActionError as e:
+            LOG.error(e)
