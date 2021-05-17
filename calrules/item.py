67b808f72f0af6245898611853ba3b013715d497
Simon <sthulb@users.noreply.github.com>
2021-05-17 15:32:45 +0100

    add more rule matchers

--- calrules/item.py
+++ calrules/item.py
@@ -1,13 +1,36 @@
-from exchangelib.items.calendar_item import BaseMeetingItem
+from enum import Enum
+from calrules.rules import Response
+from exchangelib.items.calendar_item import BaseMeetingItem, MeetingCancellation, MeetingRequest
+
+class ItemType(Enum):
+    REQUEST = 1
+    CANCELLATION = 2
+
+class ItemError(Exception):
+    pass
+
+class ItemActionError(Exception):
+    pass
 
 class Item:
+    item: BaseMeetingItem
+
+    type: ItemType
+
     subject: str
     sender: str
     recipients: list[str]
     has_conflicts: bool
-    sent_date   : int
+    sent_date: int
+    modified: bool
+    duration: str
+
+    action: Response
 
     def __init__(self, item: BaseMeetingItem = None):
+        self.action = Response.NOOP
+        self.item = item
+
         if item is not None:
             self.hydrate(item)
 
@@ -15,20 +38,42 @@ class Item:
         if item is None:
             raise ItemError('No item passed in')
 
+        if type(item) is MeetingRequest:
+            self.type = ItemType.REQUEST
+        elif type(item) is MeetingCancellation:
+            self.type = ItemType.CANCELLATION
+        else:
+            raise ItemError("Type not known")
+
         self.subject = item.subject
         self.sender = item.sender.email_address
-        self.recipients = [to.email_address for to in item.to_recipients]
-        self.has_conflicts = True if item.conflicting_meeting_count > 0 else False
-        self.duration = item.duration
+        to_recipients = item.to_recipients if item.cc_recipients is None else item.to_recipients + item.cc_recipients
+        self.recipients = [to.email_address for to in to_recipients]
+
+        if isinstance(item, MeetingRequest) and item.conflicting_meeting_count > 0:
+            self.has_conflicts = True
+        else:
+            self.has_conflicts = False
+
+        if isinstance(item, MeetingRequest):
+            self.duration = item.duration
+
         self.modified = False if item.is_unmodified else True
 
+    def mark(self, action: Response, message: str):
+        self.action = action
+        self.action_message = message
+
     def to_dict(self):
+        item_type = "REQUEST" if self.type is ItemType.REQUEST else "CANCELLATION"
+
         return {
+            'type': item_type,
             'subject': self.subject,
             'sender': self.sender,
             'recipients': self.recipients,
             'has_conflicts': self.has_conflicts,
-            'duration': self.duration,
+            # 'duration': self.duration,
             'modified': self.modified
         }
 
@@ -38,5 +83,20 @@ class Item:
         except Exception as e:
             raise ItemError(e)
 
-class ItemError(Exception):
-    pass
+    def accept(self):
+        try:
+            self.item.accept()
+        except Exception as e:
+            raise ItemError(e)
+
+    def decline(self):
+        try:
+            self.item.decline()
+        except Exception as e:
+            raise ItemActionError(e)
+
+    def maybe(self):
+        try:
+            self.item.tentatively_accept()
+        except Exception as e:
+            raise ItemActionError(e)
