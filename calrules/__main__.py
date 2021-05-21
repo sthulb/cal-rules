97b215eae95b24685128100b92812e9e454fccae
Simon <sthulb@users.noreply.github.com>
2021-05-21 14:06:37 +0100

    add args for config file

--- calrules/__main__.py
+++ calrules/__main__.py
@@ -3,6 +3,8 @@
 import logging
 import os
 
+from argparse import ArgumentParser
+
 from calrules.config import loads_config, ConfigError
 from calrules.exchange import Exchange
 from calrules.rules import Response, Rules, RuleRuntimeError
@@ -19,10 +21,20 @@ logging.basicConfig(
 
 LOG = logging.getLogger(__name__)
 
+args_parser = ArgumentParser(description="Calendar Rules")
+args_parser.add_argument("--config", help="Path to config file")
+args = args_parser.parse_args()
+
 def main():
+    config_file = "config.yaml"
+    if "config" in args and args["config"] is not None:
+        config_file = args["config"]
+
+    LOG.info(f"Using config file: {config_file}")
+
     LOG.info("Starting to scan mailbox")
     try:
-        config = loads_config("./config.yaml")
+        config = loads_config(config_file)
         LOG.debug(f"Loaded config: {config}")
     except ConfigError as e:
         LOG.error(f"Unable to parse config: {e}")
